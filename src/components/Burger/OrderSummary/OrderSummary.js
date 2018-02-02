455640ed4a7b7e5b3320f9a226c8edde05ff2e0f
Rahul <rahul999@gmail.com>
2018-02-01 19:51:29 -0500

    Added Modal for order

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Auxillary";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(mykey => {
@@ -18,6 +19,12 @@ const orderSummary = props => {
       <p>A delicious burger with following ingredients.</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
